b8e6525b1c23ff72ba3a74487a41f5b7ed3db67c
inyoung <bullgom0213@gmail.com>
2025-08-21 21:54:30 +0900

    added FRONTEND_URL to vite config allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    assetsInlineLimit: 0, // Disable inlining of assets
-  },
-  define: {
-    'import.meta.env.FRONTEND_PORT': JSON.stringify(process.env.FRONTEND_PORT || 5173),
-    'import.meta.env.BACKEND_PORT': JSON.stringify(process.env.BACKEND_PORT || 80),
-    'import.meta.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL || 'http://localhost'),
-  },
-  server: {
-    watch: {
-      usePolling: true, // Use polling for file watching
+export default defineConfig(({ mode }) => {
 
-    }
-  },
-})
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    build: {
+      assetsInlineLimit: 0, // Disable inlining of assets
+    },
+    define: {
+      'import.meta.env.FRONTEND_PORT': JSON.stringify(process.env.FRONTEND_PORT || 5173),
+      'import.meta.env.BACKEND_PORT': JSON.stringify(process.env.BACKEND_PORT || 80),
+      'import.meta.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL || 'http://localhost'),
+    },
+    server: {
+      watch: {
+        usePolling: true, // Use polling for file watching
+
+      },
+      allowedHosts: ['localhost', env.FRONTEND_URL]
+    },
+  };
+});
