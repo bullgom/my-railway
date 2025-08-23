a8dba1f6d1da287bb7c58fba7360f3104e4a18b0
inyoung <bullgom0213@gmail.com>
2025-08-23 13:54:46 +0900

    now deploy with url with https://

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [message, setMessage] = useState('');
   const [error, setError] = useState(null);
 
-  const url = `https://${import.meta.env.BACKEND_URL}:${import.meta.env.BACKEND_PORT}/`;
+  const url = `${import.meta.env.BACKEND_URL}/`;
   console.log(`Fetching data from: ${url}`);
 
   useEffect(() => {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [react(), mkcert()],
+    plugins: [react()],
     build: {
       assetsInlineLimit: 0, // Disable inlining of assets
     },
