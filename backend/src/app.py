2df178fb577dda0e86e287be03c3a5fda2c34109
inyoung <bullgom0213@gmail.com>
2025-08-23 15:07:12 +0900

    moved backed src into app

---
+++ backend/src/app.py
@@ -0,0 +1,30 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from constants import BACKEND_URL, FRONTEND_URL, BACKEND_PORT, FRONTEND_PORT
+
+
+app = FastAPI()
+
+frontend_url = f"{FRONTEND_URL}"
+origins = [
+    frontend_url,
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
