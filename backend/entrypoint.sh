627fb4ce2cd8ef427b92eeb77c54df99375083d8
inyoung <bullgom0213@gmail.com>
2025-08-23 16:05:46 +0900

    backend dockerfile base image changed to non-alpine

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ "${STAGE}" == "prod" ]; then
+    echo "Running in production mode..."
+    # Execute production-specific commands
+    hypercorn main:app --bind "[::]:${BACKEND_PORT}"
+else
+    echo "Running in development mode..."
+    # Execute development-specific commands
+    /bin/bash
+fi
